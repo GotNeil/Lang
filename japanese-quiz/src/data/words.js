d2608d9573a4e958996d8b91cfc9ccd523496304
Neil-Taiwan-1999 <neil.taiwan1999@gmail.com>
2025-09-17 18:48:54 +0800

    init git

---
+++ japanese-quiz/src/data/words.js
@@ -0,0 +1,32 @@
+
+const words = {
+  "數字 1-100": [
+    { kanji: "一", kana: "いち" },
+    { kanji: "二", kana: "に" },
+    { kanji: "三", kana: "さん" },
+    { kanji: "四", kana: "よん／し" },
+    { kanji: "五", kana: "ご" },
+    { kanji: "六", kana: "ろく" },
+    { kanji: "七", kana: "なな／しち" },
+    { kanji: "八", kana: "はち" },
+    { kanji: "九", kana: "きゅう／く" },
+    { kanji: "十", kana: "じゅう" },
+    { kanji: "十一", kana: "じゅういち" },
+    { kanji: "二十", kana: "にじゅう" },
+    { kanji: "三十", kana: "さんじゅう" },
+    { kanji: "百", kana: "ひゃく" },
+  ],
+  "數字 100-10000": [
+    { kanji: "百", kana: "ひゃく" },
+    { kanji: "二百", kana: "にひゃく" },
+    { kanji: "三百", kana: "さんびゃく" },
+    { kanji: "六百", kana: "ろっぴゃく" },
+    { kanji: "八百", kana: "はっぴゃく" },
+    { kanji: "千", kana: "せん" },
+    { kanji: "三千", kana: "さんぜん" },
+    { kanji: "八千", kana: "はっせん" },
+    { kanji: "一万", kana: "いちまん" },
+  ],
+};
+
+export default words;
